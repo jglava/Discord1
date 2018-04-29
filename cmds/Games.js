5a700d9cc2063a2fd5794e8016f1c906529b4689
jglava <josip.glavas12@gmail.com>
2018-04-29 19:05:28 +0200

    Update Games.js

--- cmds/Games.js
+++ cmds/Games.js
@@ -2,7 +2,7 @@ module.exports.run = async (client, message, args) => {
 	let games = message.guild.roles.filter(role => role.name.startsWith("$"));
 	if(games.size < 1) return message.channel.send("No games in the server yet.");
 
-	message.channel.send(games.array().join(" "));
+	message.channel.send(games.array().join("\n"));
 }
 
 module.exports.help = {
