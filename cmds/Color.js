1874f2c6f703f28f89b951ff22c6f75d13f76719
jglava <josip.glavas12@gmail.com>
2018-04-30 00:17:28 +0200

    Add files via upload

--- cmds/Color.js
+++ cmds/Color.js
@@ -1,8 +1,20 @@
+const botSettings = require("./../botsettings.json");
+const Discord = module.require("discord.js");
+
 module.exports.run = async (client, message, args) => {
 	let colors = message.guild.roles.filter(role => role.name.startsWith("#"));
 	if(colors.size < 1) return message.channel.send("No colors in the server yet.");
 
-	message.channel.send(colors.array().join(" "));
+	else{
+	 message.channel.send(colors.array().join(" "));
+	let embed = new Discord.RichEmbed()
+     		.setAuthor('Usage:')
+     		.setDescription(`${botSettings.prefix}color <name of the color without @ or #!>`)
+     		.setColor("#616163");
+
+     	message.channel.sendEmbed(embed);
+	}
+
 }
 
 module.exports.help = {
