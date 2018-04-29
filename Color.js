ff0a8861c2696bc1912abc297f30e89955467707
jglava <josip.glavas12@gmail.com>
2018-04-30 00:15:57 +0200

    Add files via upload

---
+++ Color.js
@@ -0,0 +1,25 @@
+const botSettings = require("./../botsettings.json");
+const Discord = module.require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+	let colors = message.guild.roles.filter(role => role.name.startsWith("#"));
+	if(colors.size < 1) return message.channel.send("No colors in the server yet.");
+
+	else{
+	 message.channel.send(colors.array().join(" "));
+	let embed = new Discord.RichEmbed()
+     		.setAuthor('Usage:')
+     		.setDescription(`${botSettings.prefix}color <name of the color without @ or #!>`)
+     		.setColor("#616163");
+
+     	message.channel.sendEmbed(embed);
+	}
+
+}
+
+module.exports.help = {
+	name: "colors",
+	description: 'shows the list of colors'
+
+
+}
---
+++ Games.js
@@ -0,0 +1,25 @@
+const botSettings = require("./../botsettings.json");
+const Discord = module.require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+	let games = message.guild.roles.filter(role => role.name.startsWith("$"));
+	if(games.size < 1) return message.channel.send("No games in the server yet.");
+
+		else{
+	 message.channel.send(games.array().join("\n"));
+	let embed = new Discord.RichEmbed()
+     		.setAuthor('Usage:')
+     		.setDescription(`${botSettings.prefix}game <name of the game without @ or $!>`)
+     		.setColor("#b56959");
+
+     	message.channel.sendEmbed(embed);
+	}
+
+}
+
+module.exports.help = {
+	name: "games",
+	description: 'shows the list of games'
+
+
+}
