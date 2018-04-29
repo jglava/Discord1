e26c5ba1118c6e6f05543b98e2933b39532f60a8
jglava <josip.glavas12@gmail.com>
2018-04-29 22:44:09 +0200

    Add files via upload

--- cmds/Help.js
+++ cmds/Help.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js');
 
-
 module.exports.run = async (bot, message, args) => {
   let msg = '__**Help Menu**__';
 
   bot.commands.forEach(c => {
-    msg = msg + `\n**${c.help.name}** - ${c.help.description}`;
+    msg = msg + `\n\n**${c.help.name}** - ${c.help.description}`;
   });
   msg = msg +`
-**For more help, go to psychotherapist **`;
+\n**For more help, go to psychotherapist **`;
 
   let embed = new Discord.RichEmbed()
   .setDescription(msg)
@@ -19,7 +18,6 @@ module.exports.run = async (bot, message, args) => {
   });
 
 }
-
 
 module.exports.help = {
   name: 'help',
