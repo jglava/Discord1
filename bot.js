5e8bceb3bcabc55842d17854c641a4c6487dc88c
jglava <josip.glavas12@gmail.com>
2018-04-30 11:20:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ client.on("ready", async () => {
 	} catch(e) {
 			console.log(e.stack);
 	}
-	client.user.setActivity("$help for help");
+	client.user.setActivity("type help | killing jews");
 
 });
 
