c17bf414953f8b0217b2898b5f4f1a0d403ba751
jglava <josip.glavas12@gmail.com>
2018-04-30 14:25:26 +0200

    Add files via upload

--- cmds/Gascommand.js
+++ cmds/Gascommand.js
@@ -1,12 +1,12 @@
 module.exports.run = async (client, message, args) => {
 	let target = message.mentions.users.first() || message.author;
-	await message.channel.send(` ${message.mentions.users.first() || message.author} is now being gassed by ${message.author}`);
+	if(target.id === message.author.id) return message.channel.send("You cannot gas yourself. -w-");
+	await message.channel.send(` ${message.mentions.users.first()} is now being gassed by ${message.author}`);
 
 }
 
 module.exports.help = {
-	name: "gas" || "Gas",
+	name: "gas",
 	description: 'Fun gassing someone mesasge'
 	
-
 }
