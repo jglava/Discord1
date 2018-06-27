fec1b5d753acc7f41e237857824c399d0ab98661
jglava <josip.glavas12@gmail.com>
2018-06-27 15:36:37 +0200

    Update Games.js

--- cmds/Games.js
+++ cmds/Games.js
@@ -1,15 +1,18 @@
 const botSettings = require("./../botsettings.json");
 const Discord = module.require("discord.js");
+const prefix = botSettings.prefix;
 
 module.exports.run = async (client, message, args) => {
 	let games = message.guild.roles.filter(role => role.name.startsWith("$"));
+	let description = (games.array().join("\n"));
 	if(games.size < 1) return message.channel.send("No games in the server yet.");
 
 		else{
-	 message.channel.send(games.array().join("\n"));
-	let embed = new Discord.RichEmbed()
-     		.setAuthor('Usage:')
-     		.setDescription('game <name of the game without @ or $!>')
+
+	 let embed = new Discord.RichEmbed()
+	      	.setAuthor('List of games:\n')
+     		.setDescription(description)
+     		.setFooter(`Usage: ${botSettings.prefix}game <name of the game without @ or $!>`)
      		.setColor("#b56959");
 
      	message.channel.send(embed);
